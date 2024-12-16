408030c0fc3b1afa4a056b42586baf71dc7cfd88
ABU SAID <abusaid@Abu-said17.local>
2024-12-17 00:22:15 +0600

    remove user name and password from github

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -10,9 +10,6 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-// AssetManagement
-// QbEobHFEk1OsvcXo
-// console.log("Mongodb User name ",process.env.ASSET_USER)
 const uri = `mongodb+srv://${process.env.ASSET_USER}:${process.env.ASSET_PASS}@cluster0.szzkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,25 +24,63 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const userCollection = client.db('AssetManagement').collection('UserInfo');
+    const userCollection = client.db('AssetManagement').collection('UserInfo')
+    const assetCollection =  client.db('AssetManagement').collection('AddAssets');
 
-     // using post operation for post food items on mongodb
-     app.post('/users', async (req, res) => {
+    // using post operation for post food items on mongodb
+    app.post('/users', async (req, res) => {
       const user = req.body;
       // console.log(user);
       const result = await userCollection.insertOne(user)
       res.send(result);
     })
-    app.get('/users/:email', async(req,res)=>{
+
+
+    app.get('/users/:email', async (req, res) => {
       const email = req.params.email
       const query = { email: email };
       const result = await userCollection.findOne(query)
+      // console.log(result)
       res.send(result)
     })
 
+    app.get('/all-users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
 
+    // delete operation
+    app.delete('/all-users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+    // For asset part add asset
+    app.post('/assets', async (req, res) => {
+      const asset = req.body;
+      const result = await assetCollection.insertOne(asset);
+      console.log("asset", asset);
+      res.send(result);
+
+    })
+    // delete operation
+    app.delete('/assets/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await assetCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.get('/assets', async (req, res) => {
+      const result = await assetCollection.find().toArray()
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
